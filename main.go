d7d082f90d662e8abbb884bf75fe55a1e52722dc
mattot-the-builder <mattot.builder@gmail.com>
2024-01-03 04:49:53 +0800

    Added boilerplate for assignment

--- main.go
+++ main.go
@@ -3,5 +3,32 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello mattot")
+    // create array
+
+    var response string
+
+    for {
+        fmt.Print("command> ")
+        fmt.Scan(&response)
+
+        if response == "quit" {
+            break
+        } else if response == "new" {
+            fmt.Print("Entry:")
+            fmt.Scan(&response)
+
+            // save entry to list
+
+            fmt.Println("Saving entry")
+        } else if response == "list" {
+
+            // list entries
+
+            fmt.Println("Listing entries")
+        } else {
+            fmt.Println("Unknown command", response)
+        }
+    }
+
+    fmt.Println("Bye bye")
 }
