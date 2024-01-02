35bff16f303a51ac804ed9e87467d4e3b096b12b
mattot-the-builder <mattot.builder@gmail.com>
2024-01-03 03:32:16 +0800

    Finished strconv assignment

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+func add(first int, second int) int {
+	return first + second
+}
 
 func main() {
-    fmt.Println("Hello mattot")
+	var no int = 10
+	fmt.Println(reflect.TypeOf(no))
+
+    var str string = strconv.Itoa(no)
+    fmt.Println(reflect.TypeOf(str))
+
+	var intStr string = "100"
+	fourBaseEightBitInt, _ := strconv.ParseInt(intStr, 4, 8)
+	tenBaseSixteenBitInt, _ := strconv.ParseInt(intStr, 10, 16)
+
+	fmt.Println(fourBaseEightBitInt)
+	fmt.Println(tenBaseSixteenBitInt)
+
+    // assignment
+    fmt.Println("\nAssignment")
+    no1, _ := strconv.Atoi(os.Args[1])
+    no2, _ := strconv.Atoi(os.Args[2])
+    fmt.Println(add(no1, no2))
 }
