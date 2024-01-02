c863eb1b7c9908093bfb834be0049bdc3bbd7b2e
mattot-the-builder <mattot.builder@gmail.com>
2024-01-03 03:45:21 +0800

    Finished loop assignment and challenge

--- main.go
+++ main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello mattot")
+    arr := []string{"arg1", "arg2", "arg3"}
+    for i, s := range arr {
+        fmt.Printf("index: %d, item: %s\n", i, s)
+    }
+
+    // assignment
+    fmt.Println("Assignment\n")
+    
+    var keepGoing = true
+    answer := ""
+
+    for keepGoing {
+        fmt.Println("Type command: ")
+        fmt.Scan(&answer)
+        if answer == "quit" {
+            keepGoing = false
+        } else if answer == "print" {
+            fmt.Println("Printing file")
+        }
+    }
+
+    fmt.Println("Program exit")
 }
