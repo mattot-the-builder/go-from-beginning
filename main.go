757d186ad8810e4b6fdc0bd2dbd410e49d4e9239
mattot-the-builder <mattot.builder@gmail.com>
2024-01-03 03:35:44 +0800

    Finished strconv challenge

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ func main() {
 	var no int = 10
 	fmt.Println(reflect.TypeOf(no))
 
-    var str string = strconv.Itoa(no)
-    fmt.Println(reflect.TypeOf(str))
+	var str string = strconv.Itoa(no)
+	fmt.Println(reflect.TypeOf(str))
 
 	var intStr string = "100"
 	fourBaseEightBitInt, _ := strconv.ParseInt(intStr, 4, 8)
@@ -25,9 +25,15 @@ func main() {
 	fmt.Println(fourBaseEightBitInt)
 	fmt.Println(tenBaseSixteenBitInt)
 
-    // assignment
-    fmt.Println("\nAssignment")
-    no1, _ := strconv.Atoi(os.Args[1])
-    no2, _ := strconv.Atoi(os.Args[2])
-    fmt.Println(add(no1, no2))
+	// assignment & challenge
+	fmt.Println("\nAssignment")
+	no1, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Please enter number")
+	}
+	no2, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Please enter number")
+	}
+	fmt.Println(add(no1, no2))
 }
