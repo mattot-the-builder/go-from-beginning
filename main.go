95bbcd00d6f7a5ee0eed54af0033945dba8daa2f
mattot-the-builder <mattot.builder@gmail.com>
2024-01-03 04:06:30 +0800

    Finished function practice

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func compute(first int, second int) (sum int, product int) {
+    sum = first + second
+    product = first * second
+    return
+}
+
+func log(message string) {
+    fmt.Println(message)
+}
+
 func main() {
-    fmt.Println("Hello mattot")
+    fmt.Println(compute(2,3))
+    sum, product := compute(5,10)
+    fmt.Printf("sum: %d, product: %d\n", sum, product)
+    log("Holla")
 }
