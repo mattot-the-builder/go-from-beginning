ceb8b03da39689fb11a97e246bbd09ba912a8643
mattot-the-builder <mattot.builder@gmail.com>
2024-01-03 18:19:03 +0800

    Finished maps challenge

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func store(phonebook map[string]string) {
+	var name, contact string
+
+	fmt.Print("Enter contact : ")
+	fmt.Scan(&name, &contact)
+
+	phonebook[name] = contact
+}
+
 func main() {
 	// phonebook := map[int]string{1: "john", 2: "doe", 3: "mattot"}
 	//
@@ -17,44 +26,50 @@ func main() {
 	//    _, exist := dictionaryMalayKelate["Marah"]
 	//    fmt.Println(exist)
 	//
-    //    delete(p:honebook, 2)
+	//    delete(p:honebook, 2)
 	//
 	//    for key,value := range phonebook {
 	//        fmt.Println(key, value)
 	//    }
 
 	var response string
-    phonebook := map[string]string{}
+	phonebook := map[string]string{}
 
 	for {
 		fmt.Println("Welcome to your phonebook.")
 		fmt.Print("Command> ")
 		fmt.Scan(&response)
 
-        if response == "quit" {
-            break
-        } else if response == "store" {
-            // store
+		if response == "quit" {
+			break
+		} else if response == "store" {
+			// store
+			store(phonebook)
 
-            var name, contact string
+		} else if response == "list" {
+			// list
+			for key, value := range phonebook {
+				fmt.Printf("%s %s\n", key, value)
+			}
+		} else if response == "lookup" {
+			// lookup name and number
+			fmt.Print("Enter name: ")
+			fmt.Scan(&response)
 
-            fmt.Print("Enter contact : ")
-            fmt.Scan(&name, &contact)
+			res, exist := phonebook[response]
 
-            phonebook[name] = contact
-        } else if response == "list" {
-            // list
-            for key, value := range phonebook {
-                fmt.Printf("%s %s\n", key, value)
-            }
-        } else if response == "lookup" {
-            // lookup name and number
-            fmt.Print("Enter name: ")
-            fmt.Scan(&response)
+			if exist {
+				fmt.Printf("%s has number : %s\n", response, res)
+			} else {
+				fmt.Print("Contact doesn't exist, do you want to add it? y/n :")
+				fmt.Scan(&response)
+                if response == "y" || response == "Y" {
+                    store(phonebook)
+                }
+			}
 
-            fmt.Printf("%s has number : %s\n",response ,phonebook[response])
-        }
+		}
 
 	}
-    fmt.Println("Bye bye")
+	fmt.Println("Bye bye")
 }
