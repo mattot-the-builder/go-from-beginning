952109230f3c514b624b6b4ccd6df56f65ad89e4
mattot-the-builder <mattot.builder@gmail.com>
2024-01-03 19:09:33 +0800

    Added import statement to another go package

---
+++ app/helper/helper.go
@@ -0,0 +1,7 @@
+package helper
+
+import "fmt"
+
+func Help() {
+    fmt.Println("help! haha im from helper package in app/helper")
+}
---
+++ app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/mattot-the-builder/go-from-beginning/app/helper"
+)
+
+func main() {
+    fmt.Println("Hello mattot")
+    helper.Help()
+}
