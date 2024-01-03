37a9aa4a9aa1a17356da493d98a8514fccdca46c
mattot-the-builder <mattot.builder@gmail.com>
2024-01-03 20:46:54 +0800

    Added json reading from string literal example

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Product struct {
+	Id   int    `json: "id"`
+	Name string `json: "name"`
+}
+
+type Response struct {
+	Products []Product `json: "products"`
+}
 
 func main() {
-    fmt.Println("Hello mattot")
+    str := `{ "name": "my product", "id": 1}`
+    product := Product{}
+    json.Unmarshal([]byte(str), &product)
+    fmt.Println(product)
 }
